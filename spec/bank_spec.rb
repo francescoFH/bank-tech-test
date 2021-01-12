0a47365cc523829128c67a8008619707efaa0146
francescoFH <francescofacca@gmail.com>
2021-01-12 12:47:36 +0000

    Added and tested bank withdraw of an existing account with balance

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,8 +3,18 @@ require 'bank'
 describe Bank do
   let(:account) { Bank.new }
 
-  it 'adds the deposit' do
-    account.deposit(100)
-    expect(account.balance).to eq 100
+  describe '#deposit' do
+    it 'increases the balance' do
+      account.deposit(100)
+      expect(account.balance).to eq 100
+    end
+  end
+
+  describe '#withdraw' do
+    it 'reduces the balance' do
+      account = Bank.new(100)
+      account.withdraw(30)
+      expect(account.balance).to eq 70
+    end
   end
 end
