eb50f56087366f6f78f5af218ee0e026dc29df69
francescoFH <francescofacca@gmail.com>
2021-01-12 17:09:54 +0000

    Added and tested show statement method that displays the activity report

---
+++ lib/statement.rb
@@ -0,0 +1,24 @@
+require_relative 'bank'
+
+class Statement
+
+  def show(values = false)
+      if values
+        statement = statement(values)
+        puts statement
+        return statement
+      else header
+      end
+  end
+
+  private
+  def header
+    'date || credit || debit || balance'
+  end
+
+  def statement(values)
+  values.map! { |entry| entry.join(" || ") }
+  reversed_entries = values.reverse.join("\n ")
+  "#{header}\n #{reversed_entries}"
+end
+end
---
+++ spec/statement_spec.rb
@@ -0,0 +1,17 @@
+require 'statement'
+
+describe Statement do
+  let(:statement) { Statement.new }
+  let(:account) { Bank.new }
+  let(:date) { Time.new.strftime('%d/%m/%Y') }
+
+  it 'displays an empty statement' do
+    expect(statement.show()).to eq 'date || credit || debit || balance'
+  end
+
+  it 'displays activity_report' do
+    account.deposit(100)
+    account.withdraw(30)
+    expect(statement.show(account.activity_report)).to eq "date || credit || debit || balance\n #{date} ||  || 30 || 70\n #{date} || 100 ||  || 100"
+  end
+end
