b7f5bfd844380e444c4060cf1ef72209bd7003bd
francescoFH <francescofacca@gmail.com>
2021-01-12 16:08:33 +0000

    Added and tested activity report that stores money movements

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,18 +1,21 @@
 class Bank
 
-  attr_reader :balance
+  attr_reader :balance, :activity_report
 
   def initialize(balance = 0)
     @balance = balance
+    @activity_report = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date = Time.new.strftime('%d/%m/%Y'))
     @balance += amount
+    @activity_report << [date, amount, nil, balance]
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date = Time.new.strftime('%d/%m/%Y'))
     fail 'Unable to withdraw: insufficient funds.' if (@balance - amount).negative?
     @balance -= amount
+    @activity_report << [date, nil, amount, balance]
   end
 
   private
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,6 +2,7 @@ require 'bank'
 
 describe Bank do
   let(:account) { Bank.new }
+  let(:date) { Time.new.strftime('%d/%m/%Y') }
 
   describe '#deposit' do
     it 'increases the balance' do
@@ -19,6 +20,14 @@ describe Bank do
 
     it 'raise error if overdrawn' do
       expect { account.withdraw(10) }.to raise_error 'Unable to withdraw: insufficient funds.'
+    end
+  end
+
+  describe '#activity_report' do
+    it 'stores money movements' do
+      account.deposit(100)
+      account.withdraw(30)
+      expect(account.activity_report).to eq([[date, 100, nil, 100], [date, nil, 30, 70]])
     end
   end
 end
